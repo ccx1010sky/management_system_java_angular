445e2332ec6564767709c5caae8a43eca6cd6d7e
ccx1010sky <ccx1010@gmail.com>
2022-10-23 21:28:14 +0100

    populate with dummy data and be able to display on browser

--- angular_frontend/src/app/employee-list/employee-list.component.ts
+++ angular_frontend/src/app/employee-list/employee-list.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Employee } from "../employee";
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent implements OnInit {
+  employees: Employee[];
 
-  employees:Employee[];
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // adding dummy data
+    this.employees = [
+      {
+        id: 1,
+        firstName: 'Michael',
+        lastName: 'Jackson',
+        emailId: 'michael@gmail.com',
+      },
+      {
+        id: 2,
+        firstName: 'John',
+        lastName: 'Conner',
+        emailId: 'john@gmail.com',
+      },
+    ];
   }
-
 }
