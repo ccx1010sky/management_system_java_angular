12faf0b79cb55d20b433fa3227d8a52bab1237c2
ccx1010sky <ccx1010@gmail.com>
2022-10-29 14:55:13 +0100

    delete by id method

--- springboot_backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
+++ springboot_backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.springboot_backend.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import com.example.springboot_backend.exception.ResourceNotFoundException;
@@ -56,7 +58,16 @@ public class EmployeeController {
         Employee updatedEmployee = employeeRepository.save(employee);
         return ResponseEntity.ok(updatedEmployee);
 }
-
+    //delete employee rest api
+    @DeleteMapping("/employees/{id}")
+     public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 
 
 
