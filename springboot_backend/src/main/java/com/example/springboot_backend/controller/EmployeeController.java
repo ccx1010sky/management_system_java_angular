522ba9a2fe73d6d90888e0a1884fd4f1710f4961
ccx <cx@mbp-computer.home>
2022-11-20 16:46:18 +0000

    cached remove /target folder and now is working

---
+++ springboot_backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
@@ -0,0 +1,76 @@
+package com.example.springboot_backend.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.springboot_backend.exception.ResourceNotFoundException;
+import com.example.springboot_backend.repository.EmployeeRepository;
+import com.example.springboot_backend.model.Employee;
+
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+//RestController is middle layer between JPA and MySQL
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    //     EmployeeRepository接口声明并实例化employeeRepository, Autowired 工厂生产实例 dependency injection并注入
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //接口下的get, post update,delete各种方法：
+    //1,get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+        //http://localhost:8080/api/v1/employees
+    }
+
+    // 2, create employee restful api and save to MySQL database
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    //3,get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    //update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+}
+    //delete employee rest api
+    @DeleteMapping("/employees/{id}")
+     public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+
+
+
+}
+
