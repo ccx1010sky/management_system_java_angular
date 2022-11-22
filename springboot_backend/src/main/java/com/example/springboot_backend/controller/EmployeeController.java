15e4ac92c43222d0ae0f7507e3dcd6a9753deae0
ccx <cx@mbp-computer.home>
2022-11-22 20:46:48 +0000

    change cors: allow origin(http://ngfrontendcode.s3-website.eu-west-2.amazonaws.com) to load and send request to cross-origin(http://aab-env.eba-hhay3gnm.eu-west-2.elasticbeanstalk.com)

--- springboot_backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
+++ springboot_backend/src/main/java/com/example/springboot_backend/controller/EmployeeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 //RestController is middle layer between JPA and MySQL
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://ngfrontendcode.s3-website.eu-west-2.amazonaws.com")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 
